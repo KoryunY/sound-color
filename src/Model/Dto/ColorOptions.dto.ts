d179188f6a64ff7cd1ebf08dc08e0549dca00409
KoryunY <yeritsyankoryun@gmail.com>
2023-04-13 23:50:32 +0400

    endpoints and constss

--- src/Model/Dto/ColorOptions.dto.ts
+++ src/Model/Dto/ColorOptions.dto.ts
@@ -4,9 +4,9 @@ import { ConvertingType } from "src/Defaults/types";
 
 export interface ColorOptionsDto {
     type: ConvertingType,
-    //data?: string | number,
     saturation?: number | 100,
     ligthness?: number | 50,
-    intervalCount: number | 64,
-    userId:ObjectId
+    intervalCount?: number | 64,
+    user: ObjectId,
+    config?: ObjectId
 }
